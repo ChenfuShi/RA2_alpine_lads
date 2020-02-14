be3c4e2c18c897fb0c29d821953013688fd92067
Michael Stadler <mbaxtms3@hlogin2.pri.csf3.alces.network>
2020-02-14 15:50:00 +0000

    Fixes in train.py

--- train/train.py
+++ train/train.py
@@ -13,7 +13,7 @@ def train_feet_erosion_model(config, pretrained_base_model):
     pretrained_base_model.compile(loss = 'categorical_crossentropy', metrics=["categorical_accuracy", top_2_categorical_accuracy], optimizer='adam')
 
     history = pretrained_base_model.fit(
-        feet_joint_erosion_dataset, epochs = 25, steps_per_epoch = 200, validation_data = val_dataset, validation_steps = 10, class_weight = dataset.class_weights
+        feet_joint_erosion_dataset, epochs = 25, steps_per_epoch = 200, validation_data = val_dataset, validation_steps = 5, class_weight = dataset.class_weights
     )
 
     return pretrained_base_model
