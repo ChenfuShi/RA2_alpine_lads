768bc3f94b7d6424469f6e02f5fb569e5aa8b7bb
chenfu.shi@postgrad.manchester.ac.uk <https://chenfushi@github.com>
2020-02-29 21:40:39 +0000

    added complex joint model pretrain NIH

--- ra_joint_predictions/model/NIH_model.py
+++ ra_joint_predictions/model/NIH_model.py
@@ -13,6 +13,7 @@ import os
 import tensorflow as tf
 import tensorflow_addons as tfa
 from model.utils.keras_nasnet import NASNet
+from model.utils.building_blocks_joints import create_complex_joint_model
 
 def create_resnet_multioutput(config):
     base_resnet = keras.applications.resnet_v2.ResNet50V2(input_shape=[config.landmarks_img_height,config.landmarks_img_width,1],pooling="avg",weights=None,)
@@ -45,8 +46,38 @@ def create_resnet_multioutput(config):
     return model
 
 
+def create_complex_joint_multioutput(config):
 
 
+    inputs = keras.layers.Input(shape=[config.img_height,config.img_width,1])
+
+    # create new model with common part
+    common_part = create_complex_joint_model(inputs)
+
+    # split into two parts
+    disease = keras.layers.Dense(14, activation='sigmoid', name='disease_pred')(common_part)
+    sex = keras.layers.Dense(1, activation='sigmoid', name='sex_pred')(common_part)
+    age = keras.layers.Dense(1,activation="linear",name="age_pred")(common_part)
+
+    # get final model
+    model = keras.models.Model(
+        inputs=inputs,
+        outputs=[disease,sex, age],
+        name="resnet_multiout_NIH")
+
+    losses = {
+    "disease_pred": tfa.losses.focal_loss.SigmoidFocalCrossEntropy(),
+    "sex_pred" : "binary_crossentropy",
+    "age_pred": "mean_squared_error",
+    }
+    lossWeights = {"disease_pred": 2.0,"sex_pred" :0.5,"age_pred": 0.005}
+
+    #cl_weights = { 'disease_pred': {0: 1 , 1: 1, 2: 1, 3: 1, 4: 1, 5: 1, 6: 1, 7: 1, 8: 1, 9: 1, 10: 1, 11: 1, 12: 1, 13: 1, 14: 1}}
+
+    model.compile(optimizer='adam', loss=losses, loss_weights=lossWeights, metrics={"disease_pred":"binary_accuracy","sex_pred":"binary_accuracy","age_pred":"mae"},)
+    #class_weight = cl_weights)
+
+    return model
 
 
 def create_NASnet_multioutupt(config):
