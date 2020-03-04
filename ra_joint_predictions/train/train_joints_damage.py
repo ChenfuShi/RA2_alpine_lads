bc0dee0525c4534b1e89252a71c174585191b4a8
Michael <michael.stadler@postgrad.manchester.ac.uk>
2020-03-04 11:23:30 +0000

    Added scripts for model training

--- ra_joint_predictions/train/train_joints_damage.py
+++ ra_joint_predictions/train/train_joints_damage.py
@@ -7,33 +7,47 @@ import tensorflow as tf
 
 from tensorflow.keras.layers import Dense
 
-from dataset.joint_dataset import feet_joint_dataset
+from dataset.joint_dataset import feet_joint_dataset, hands_joints_dataset, hands_wrists_dataset
 from model.utils.metrics import top_2_categorical_accuracy
 from model.utils.metrics import argmax_rsme, softmax_rsme_metric, class_softmax_rsme_metric
 from utils.saver import CustomSaver, _get_tensorboard_callback
 
-def train_feet_narrowing_model(config, model_name, pretrained_base_model):
+def train_joints_damage_model(config, model_name, pretrained_base_model, joint_type, dmg_type):
     saver = CustomSaver(model_name, n = 10)
     tensorboard_callback = _get_tensorboard_callback(model_name)
     
-    dataset = feet_joint_dataset(config)
+    joint_dataset, tf_joint_dataset = _get_dataset(config, joint_type, dmg_type)
     
-    outcomes_sources = os.path.join(config.train_location, 'training.csv')
-    
-    feet_joint_erosion_dataset = dataset.create_feet_joints_dataset(outcomes_sources, joints_source = './data/predictions/feet_joint_data.csv')
-
-    # output_bias = tf.keras.initializers.Constant(dataset.class_bias[0])
-    
-    pretrained_base_model.add(Dense(5, activation = 'softmax', name = 'main_output')) # , bias_initializer = output_bias))
+    pretrained_base_model.add(Dense(5, activation = 'softmax', name = 'main_output'))
 
     metrics = ['categorical_accuracy', softmax_rsme_metric(np.arange(5)), argmax_rsme, class_softmax_rsme_metric(np.arange(5), 0)]
     pretrained_base_model.compile(loss = 'categorical_crossentropy', metrics = metrics, optimizer = 'adam')
 
     history = pretrained_base_model.fit(
-        feet_joint_erosion_dataset, epochs = 100, steps_per_epoch = 75, verbose = 2, class_weight = dataset.class_weights[0], callbacks = [saver, tensorboard_callback]
+        tf_joint_dataset, epochs = 100, steps_per_epoch = 75, verbose = 2, class_weight = joint_dataset.class_weights[0], callbacks = [saver, tensorboard_callback]
     )
 
     hist_df = pd.DataFrame(history.history)
     hist_df.to_csv('./' + model_name + '_hist.csv')
 
     return pretrained_base_model
+
+def _get_dataset(config, joint_type, dmg_type):
+    outcomes_sources = os.path.join(config.train_location, 'training.csv')
+
+    erosion_flag = dmg_type == 'E'
+
+    if joint_type == 'F':
+        joint_dataset = feet_joint_dataset(config)
+
+        tf_joint_dataset = joint_dataset.create_feet_joints_dataset(outcomes_sources, joints_source = './data/predictions/feet_joint_data.csv', erosion_flag = erosion_flag)
+    elif joint_type == 'H':
+        joint_dataset = hands_joints_dataset(config)
+
+        tf_joint_dataset = joint_dataset.create_hands_joints_dataset(outcomes_sources, joints_source = './data/predictions/hand_joint_data.csv', erosion_flag = erosion_flag)
+    else:
+        joint_dataset = hands_wrists_dataset(config)
+
+        tf_joint_dataset = joint_dataset.create_wrists_joints_dataset(outcomes_sources, joints_source = './data/predictions/hand_joint_data.csv', erosion_flag = erosion_flag)
+
+    return joint_dataset, tf_joint_dataset
--- ra_joint_predictions/train_joint_model_script.py
+++ ra_joint_predictions/train_joint_model_script.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 from utils.config import Config
 from utils.saver import save_pretrained_model
 
-from train.train_joints_damage import train_feet_narrowing_model
+from train.train_joints_damage import train_joints_damage_model
 
 if __name__ == '__main__':
     os.chdir('/mnt/jw01-aruk-home01/projects/ra_challenge/RA_challenge/michael_dev/RA2_alpine_lads/ra_joint_predictions')
@@ -18,12 +18,17 @@ if __name__ == '__main__':
     model_name = sys.argv[2]
     logging.info('Saving trained model to: %s', model_name)
 
+    # F for feet, H for hands, W for wrists
+    joint_type = sys.argv[3]
+    # E for Erosion, J for narrowing
+    dmg_type = sys.argv[4]
+
     logging.info('Loading config')
     config = Config()
 
     # load pretrained model
     loaded_model = tf.keras.models.load_model(pretrained_model + '.h5')
 
-    trained_model = train_feet_narrowing_model(config, model_name, loaded_model)
+    trained_model = train_joints_damage_model(config, model_name, loaded_model, joint_type, dmg_type)
 
     save_pretrained_model(trained_model, 0, model_name)
