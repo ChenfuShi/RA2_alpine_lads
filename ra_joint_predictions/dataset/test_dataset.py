06af68d06a28578bc12fd415f2244c14ae6b4ceb
Michael <michael.stadler@postgrad.manchester.ac.uk>
2020-03-04 17:13:31 +0000

    Added predictions for docker

---
+++ ra_joint_predictions/dataset/test_dataset.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import tensorflow as tf
+
+import dataset.ops.image_ops as img_ops
+import dataset.joint_dataset as joint_dataset
+import dataset.ops.joint_ops as joint_ops
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+class joint_test_dataset(joint_dataset.joint_dataset):
+    def __init__(self, config, img_dir):
+        super().__init__(config)
+
+        self.img_dir = img_dir
+
+    def get_feet_joint_test_dataset(self, joints_source = './data/predictions/feet_joint_data_test.csv'):
+        return self._create_joint_dataset(joints_source, joint_dataset.foot_outcome_mapping.keys())
+
+    def get_hand_joint_test_dataset(self, joints_source = './data/predictions/hand_joint_data_test.csv'):
+        return self._create_joint_dataset(joints_source, joint_dataset.hand_outcome_mapping.keys())
+
+    def get_wrist_joint_testdataset(self, joints_source = './data/predictions/hand_joint_data_test.csv'):
+        return self._create_joint_dataset(joints_source, joint_dataset.hand_joint_keys, load_wrists = True)
+
+    def _create_joint_dataset(self, joints_source, joint_keys, load_wrists = False):
+        joints_df = self._create_intermediate_joints_df(joints_source, joint_keys)
+        
+        file_info = joints_df[['image_name', 'file_type', 'flip', 'key']].to_numpy()
+        
+        if load_wrists:
+            joint_coords = joints_df[['w1_x', 'w1_y', 'w2_x', 'w2_y', 'w3_x', 'w3_y']].to_numpy()
+        else:
+            joint_coords = joints_df[['coord_x', 'coord_y']].to_numpy()
+
+        dataset = tf.data.Dataset.from_tensor_slices((file_info, joint_coords))
+
+        if load_wrists:
+            dataset = self._load_wrists_without_outcomes(dataset)
+        else:
+            dataset = self._load_joints_without_outcomes(dataset)
+
+        dataset = self._resize_images_without_outcomes(dataset)
+
+        return dataset.prefetch(buffer_size = AUTOTUNE)
+        
+    def _load_joints_without_outcomes(self, dataset):
+        def __load_joints(file_info, y):
+            x_coord = y[0]
+            y_coord = y[1]
+
+            full_img, _ = img_ops.load_image(file_info, [], self.img_dir)
+
+            joint_img = joint_ops._extract_joint_from_image(full_img, x_coord, y_coord)
+
+            return file_info, joint_img
+
+        return dataset.map(__load_joints, num_parallel_calls=AUTOTUNE)
+
+    def _load_wrists_without_outcomes(self, dataset):
+        def __load_wrists(file_info, y):
+            w1_x = y[0]
+            w2_x = y[2]
+            w3_x = y[4]
+            w1_y = y[1]
+            w2_y = y[3]
+            w3_y = y[5]
+
+            full_img, _ = img_ops.load_image(file_info, [], self.img_dir)
+
+            joint_img = joint_ops._extract_wrist_from_image(full_img, w1_x, w2_x, w3_x, w1_y, w2_y, w3_y)
+
+            return file_info, joint_img
+
+        return dataset.map(__load_wrists, num_parallel_calls=AUTOTUNE)
+
+    def _resize_images_without_outcomes(self, dataset):
+        def __resize(file_info, img):
+            img, _ =  img_ops.resize_image(img, [], self.config.joint_img_height, self.config.joint_img_width, pad_resize = False, update_labels = False)
+
+            return file_info, img
+
+        return dataset.map(__resize, num_parallel_calls=AUTOTUNE)
--- ra_joint_predictions/dream/prediction.py
+++ ra_joint_predictions/dream/prediction.py
@@ -1,24 +1,139 @@
 import logging
+
 import numpy as np
 import pandas as pd
+import tensorflow as tf
+
+import dataset.joint_dataset as joint_dataset
+
+from model.utils.metrics import argmax_rsme, softmax_rsme_metric, class_softmax_rsme_metric
+from dataset.test_dataset import joint_test_dataset
 
 def predict_dream_test_set(config):
-	training_data = pd.read_csv('/train/training.csv')
-	
-	mean_preds = np.mean(training_data.iloc[:, 1:], axis = 0)
+    feet_narrowing_model, hands_narrowing_model, wrists_narrowing_model, hands_erosion_model, feet_erosion_model, wrists_erosion_model = _get_models()
+    
+    preds = {}
+    
+    def _init_preds(patient_id):
+        if not patient_id in preds.keys():
+            preds[patient_id] = {
+                'Patient_ID': patient_id
+        }
+            
+    dataset = joint_test_dataset(config, config.train_fixed_location)
 
-	val_dict = {}
-	for idx, col in enumerate(training_data.columns[1:]):
-		val_dict[col] = mean_preds[idx]
+    hands_test_dataset = dataset.get_hands_test_dataset(joints_source = '/output/dream_test_hand_joint_data.csv')
+    wrists_test_dataset = dataset.get_wrist_joint_testdataset(joints_source = '/output/dream_test_hand_joint_data.csv')
+    feet_test_dataset = dataset.get_feet_joint_test_dataset(joints_source = '/output/dream_test_feet_joint_data.csv')
 
-	pred_dicts = []
+    for file_info, img in hands_test_dataset:
+        patient_id, part, key = _get_details(file_info)
+        
+        __init_preds(patient_id)
+        
+        img_tf = tf.expand_dims(img, 0)
+        
+        narrowing_outcome_key = joint_dataset.hand_outcome_mapping[key][0][0].format(part = part)
+        y_pred = feet_narrowing_model.predict(img_tf)
+        y_pred = np.sum(y_pred * np.arange(5))
+        preds[patient_id][narrowing_outcome_key] = y_pred
+        
+        erosion_outcome_key = joint_dataset.hand_outcome_mapping[key][1][0].format(part = part)
+        y_pred = feet_erosion_model.predict(img_tf)
+        y_perd = np.sum(y_pred * np.arange(6))
+        preds[patient_id][erosion_outcome_key] = y_pred
+        
+    for file_info, img in wrists_test_dataset:
+        patient_id, part, key = _get_details(file_info)
+        
+        __init_preds(patient_id)
+        
+        img_tf = tf.expand_dims(img, 0)
+        
+        wrist_narrowing_outcome_keys = [wrist_key.format(part = part) for wrist_key in joint_dataset.wrist_outcome_mapping['wirst'][0]]
+        wrist_erosion_outcome_keys = [wrist_key.format(part = part) for wrist_key in joint_dataset.wrist_outcome_mapping['wirst'][1]]
+        
+        y_pred = wrists_narrowing_model.predict(img_tf)
+        for idx, narrowing_key in enumerate(wrist_narrowing_outcome_keys):
+            preds[patient_id][narrowing_key] = np.sum(y_pred[idx] * np.range(5))
+            
+        y_pred = wrists_erosion_model.predict(img_tf)
+        for idx, erosion_key in enumerate(wrist_erosion_outcome_keys):
+            preds[patient_id][erosion_key] = np.sum(y_pred[idx] * np.range(11))
+    
+    for file_info, img in feet_test_dataset:
+        patient_id, part, key = _get_details(file_info)
+        
+        __init_preds(patient_id)
+        
+        img_tf = tf.expand_dims(img, 0)
+        
+        narrowing_outcome_key = joint_dataset.foot_outcome_mapping[key][0][0].format(part = part)
+        y_pred = feet_narrowing_model.predict(img_tf)
+        y_pred = np.sum(y_pred * np.arange(5))
+        preds[patient_id][narrowing_outcome_key] = y_pred
+        
+        erosion_outcome_key = joint_dataset.foot_outcome_mapping[key][1][0].format(part = part)
+        y_pred = feet_erosion_model.predict(img_tf)
+        y_perd = np.sum(y_pred * np.arange(11))
+        preds[patient_id][erosion_outcome_key] = y_pred
+        
+    predictions_df = pd.DataFrame(preds.values(), index = np.arange(len(preds.values())))
+    
+    narrowing_mask = ['_J_' in column_name for column_name in predictions_df.columns]
+    erosion_mask = ['_E_' in column_name for column_name in predictions_df.columns]
+    
+    narrowing_sum = np.sum(predictions_df.iloc[:, narrowing_mask].to_numpy(), axis = 1)
+    erosion_sum = np.sum(predictions_df.iloc[:, erosion_mask].to_numpy(), axis = 1)
+    
+    total_sum = narrowing_sum + erosion_sum
+    
+    predictions_df['Overall_narrowing'] = narrowing_sum
+    predictions_df['Overall_erosion'] = erosion_sum
+    predictions_df['Overall_Tol'] = total_sum
+    
+    template = pd.read_csv('/test/template.csv')
+    predictions_df = predictions_df[template.columns]
+    predictions_df.to_csv('/output/predictions.csv', index = False)
+            
+def _get_models():
+    dependencies = {
+        'softmax_rsme': softmax_rsme_metric(np.arange(5)),
+        'argmax_rsme': argmax_rsme,
+        'class_softmax_rmse_0': class_softmax_rsme_metric(np.arange(5), 0)
+    }
 
-	template = pd.read_csv('/test/template.csv')
-	for idx, row in template.iterrows():
-    		pred_dict = val_dict.copy()
-    		pred_dict['Patient_ID'] = row['Patient_ID']
+    feet_narrowing_model = tf.keras.models.load_model('feet_narrowing_v1.h5', custom_objects=dependencies)
+    hands_narrowing_model = tf.keras.models.load_model('hands_narrowing_v1.h5', custom_objects=dependencies)
+    wrists_narrowing_model = tf.keras.models.load_model('wrists_narrowing_v1.h5', custom_objects=dependencies)
+    
+    dependencies = {
+        'softmax_rsme': softmax_rsme_metric(np.arange(6)),
+        'argmax_rsme': argmax_rsme,
+        'class_softmax_rmse_0': class_softmax_rsme_metric(np.arange(6), 0)
+    }
 
-    		pred_dicts.append(pred_dict)
+    hands_erosion_model = tf.keras.models.load_model('hands_erosion_v1.h5', custom_objects=dependencies)
+    wrists_erosion_model = tf.keras.models.load_model('wrists_erosion_v1.h5', custom_objects=dependencies)
+    
+    dependencies = {
+        'softmax_rsme': softmax_rsme_metric(np.arange(11)),
+        'argmax_rsme': argmax_rsme,
+        'class_softmax_rmse_0': class_softmax_rsme_metric(np.arange(11), 0)
+    }
+                       
+    feet_erosion_model = tf.keras.models.load_model('feet_erosion_v1.h5', custom_objects=dependencies)
 
-	predictions = pd.DataFrame(pred_dicts, index = np.arange(len(pred_dicts)), columns = template.columns)
-	predictions.to_csv('/output/predictions.csv', index = False)
+    return feet_narrowing_model, hands_narrowing_model, wrists_narrowing_model, hands_erosion_model, feet_erosion_model, wrists_erosion_model
+                       
+def _get_details(file_info):
+    file_info = file_info.numpy()
+    
+    img_info = file_info[0].decode('utf-8').split('.')[0]
+    key = file_info[3].decode('utf-8')
+    
+    patient_info = img_info.split('-')
+    patient_id = patient_info[0]
+    part = patient_info[1]
+        
+    return patient_id, part, key
