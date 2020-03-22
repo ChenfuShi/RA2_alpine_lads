0147b3f8a58d5dcec4e953a4269091aa3884b4f7
Michael <michael.stadler@postgrad.manchester.ac.uk>
2020-03-22 00:08:35 +0000

    Fixed load joint damage model

--- ra_joint_predictions/model/joint_damage_model.py
+++ ra_joint_predictions/model/joint_damage_model.py
@@ -2,31 +2,16 @@ import numpy as np
 
 import tensorflow.keras as keras
 
-from model.utils.metrics import _rmse, argmax_rmse, softmax_rmse_metric, class_filter_softmax_rmse_metric, rmse_metric, class_rmse_metric, mae_metric, class_filter_rmse_metric, softmax_mae_metric
+from model.utils.metrics import mae_metric, rmse_metric, class_filter_rmse_metric, softmax_mae_metric, softmax_rmse_metric, class_filter_softmax_rmse_metric
+
 from model.utils.building_blocks_joints import get_joint_model_input, create_complex_joint_model
 
 MODEL_TYPE_CLASSIFICATION = "C"
 MODEL_TYPE_REGRESSION = "R"
 MODEL_TYPE_COMBINED = "RC"
 
-def load_joint_damage_model(model_file, no_classes, model_type = False):
-    if model_type == MODEL_TYPE_CLASSIFICATION:
-        dependencies = {
-            'softmax_rmse': softmax_rmse_metric(np.arange(no_classes)),
-            'class_softmax_rmse_0': class_filter_softmax_rmse_metric(np.arange(no_classes), 0),
-            'argmax_rmse': argmax_rmse,
-            'class_softmax_rsme_0': class_filter_softmax_rmse_metric(np.arange(no_classes), 0), # Added for compatibility with models saved with the previous spelling mistake
-            'softmax_rsme': softmax_rmse_metric(np.arange(no_classes)) # Added for compatibility with models saved with the previous spelling mistake
-        }
-    else:
-        dependencies = {
-            'rmse': _rmse
-        }
-
-        for n in range(no_classes):
-            dependencies[f'class_{n}_rmse'] = class_rmse_metric(n)
-
-    return keras.models.load_model(model_file, custom_objects = dependencies)
+def load_joint_damage_model(model_file):
+    return keras.models.load_model(model_file, compile = False)
 
 def get_joint_damage_model(config, class_weights, pretrained_model_file = None, model_name = 'joint_damage_model', optimizer = 'adam', model_type = 'R'):
     base_input, base_ouptut = _get_base_model(config, pretrained_model_file)
@@ -84,11 +69,11 @@ def _add_outputs(class_weights, base_output, model_type):
             metrics_dir[f'reg_output_{idx}'] = [mae_metric(max_outcome), rmse_metric(max_outcome), class_filter_rmse_metric(max_outcome, 0)]
         
         if 'C' in model_type:
-            [softmax_mae_metric(np.arange(no_outcomes)), softmax_rmse_metric(np.arange(no_outcomes)), class_softmax_rmse_metric(np.arange(no_outcomes), 0)]
+            [softmax_mae_metric(np.arange(no_outcomes)), softmax_rmse_metric(np.arange(no_outcomes)), class_filter_softmax_rmse_metric(np.arange(no_outcomes), 0)]
 
             output = keras.layers.Dense(no_outcomes, activation = 'softmax', name = f'class_output_{idx}')(base_output)
             outputs.append(output)
 
-            metrics_dir[f'class_output_{idx}'] = [softmax_mae_metric(np.arange(no_outcomes)), softmax_rmse_metric(np.arange(no_outcomes)), class_softmax_rmse_metric(np.arange(no_outcomes), 0)]
+            metrics_dir[f'class_output_{idx}'] = [softmax_mae_metric(np.arange(no_outcomes)), softmax_rmse_metric(np.arange(no_outcomes)), class_filter_softmax_rmse_metric(np.arange(no_outcomes), 0)]
 
     return outputs, metrics_dir
