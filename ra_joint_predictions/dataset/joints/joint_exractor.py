6de953467375881ef897fb96af76e00a87d15103
Michael <michael.stadler@postgrad.manchester.ac.uk>
2020-03-26 21:12:14 +0000

    Prepared new submission

--- ra_joint_predictions/dataset/joints/joint_exractor.py
+++ ra_joint_predictions/dataset/joints/joint_exractor.py
@@ -9,10 +9,10 @@ def default_joint_extractor(joint_scale = 5):
 
     return _default_joint_extractor
 
-def feet_joint_extractor(main_joint_scale = 6, ntp_joint_scale = 3):
+def feet_joint_extractor(main_joint_scale = 6, mtp_joint_scale = 3):
     def _feet_joint_extractor(img_shape, joint_key):
         if joint_key == 'mtp' or joint_key == 'mtp_1':
-            joint_scale = ntp_joint_scale
+            joint_scale = mtp_joint_scale
         
         joint_scale = tf.cast(joint_scale, tf.float64)
     
