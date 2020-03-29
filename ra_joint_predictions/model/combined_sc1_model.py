963cebae1f9b53730b363f7a4f2554c34195ee4d
chenfu.shi@postgrad.manchester.ac.uk <https://chenfushi@github.com>
2020-03-29 11:53:58 +0100

    combined model and new wrists

---
+++ ra_joint_predictions/model/combined_sc1_model.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+from tensorflow import keras
+
+
+def get_feet_model(base_model_joints_loc):
+
+    base_model_joints = keras.models.load_model(base_model_joints_loc, compile = False)
+    base_model_joints_no_dense = keras.models.Model(base_model_joints.inputs, base_model_joints.layers[-14].output) 
+    base_model_joints_no_dense.trainable = False
+
+    inputs = []
+    for i in range(6):
+        inputs.append(keras.layers.Input(shape=(224,224,1)))
+    outs = []
+    for i in range(6):
+        outs.append(base_model_joints_no_dense(inputs[i]))
+
+    pred = keras.layers.Concatenate()(outs)
+    pred = keras.layers.Dense(512, activation = "relu")(pred)
+    pred = keras.layers.BatchNormalization()(pred)
+    pred = keras.layers.Dropout(0.5)(pred)
+    pred = keras.layers.Dense(256, activation = "relu")(pred)
+    pred = keras.layers.BatchNormalization()(pred)
+    pred = keras.layers.Dropout(0.5)(pred)
+    pred = keras.layers.Dense(1)(pred)
+
+    combined_model = keras.models.Model(inputs=inputs, outputs=[pred])
+
+    combined_model.compile(optimizer = "adam", loss = "mean_squared_error", metrics = ["mae"])
+
+    return combined_model
+
+
+def get_hand_model(base_model_joints_loc, base_model_wrist_loc):
+
+    individual_model_joints = keras.models.load_model(base_model_joints_loc, compile = False)
+    individual_model_joints_no_dense = keras.models.Model(individual_model_joints.inputs, individual_model_joints.layers[-14].output) 
+    individual_model_joints_no_dense.trainable = False
+    individual_model_wrist = keras.models.load_model(base_model_wrist_loc, compile = False)
+    individual_model_wrist_no_dense = keras.models.Model(individual_model_wrist.inputs, individual_model_wrist.layers[-14].output) 
+    individual_model_wrist_no_dense.trainable = False
+
+    inputs = []
+    for i in range(11):
+        inputs.append(keras.layers.Input(shape=(224,224,1)))
+    outs = []
+    for i in range(10):
+        outs.append(individual_model_joints_no_dense(inputs[i]))
+    outs.append(individual_model_wrist_no_dense(inputs[10]))
+
+    pred = keras.layers.Concatenate()(outs)
+    pred = keras.layers.Dense(512, activation = "relu")(pred)
+    pred = keras.layers.BatchNormalization()(pred)
+    pred = keras.layers.Dropout(0.5)(pred)
+    pred = keras.layers.Dense(256, activation = "relu")(pred)
+    pred = keras.layers.BatchNormalization()(pred)
+    pred = keras.layers.Dropout(0.5)(pred)
+    pred = keras.layers.Dense(1)(pred)
+
+    combined_model = keras.models.Model(inputs=inputs, outputs=[pred])
+
+    combined_model.compile(optimizer = "adam", loss = "mean_squared_error", metrics = ["mae"])
+
+    return combined_model
