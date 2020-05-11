0785ed9e5d02e47522105062375afd615db4a0c4
Michael Stadler <michael.stadler@postgrad.manchester.ac.uk>
2020-05-11 07:56:58 +0100

    Commit wrist script

--- ra_joint_predictions/finetune_NIH_model_wrists_complex_on_RSNA.py
+++ ra_joint_predictions/finetune_NIH_model_wrists_complex_on_RSNA.py
@@ -1,3 +1,8 @@
+import tensorflow as tf
+
+tf.config.threading.set_intra_op_parallelism_threads(8)
+tf.config.threading.set_inter_op_parallelism_threads(8)
+
 from utils.config import Config
 import model
 import os
@@ -14,10 +19,10 @@ os.chdir('/mnt/jw01-aruk-home01/projects/ra_challenge/RA_challenge/michael_dev/R
 configuration = Config()
 
 ## joints
-wrist_dataset, wrist_val_dataset = rsna_wrist_dataset(configuration, pad_resize = True).create_rsna_wrist_dataset(val_split = True)
+wrist_dataset, wrist_val_dataset = rsna_wrist_dataset(configuration, pad_resize = False).create_rsna_wrist_dataset(val_split = True)
 
-model = RSNA_model.complex_joint_finetune_model(configuration,weights="weights/NIH_new_pretrain_model_0.h5", no_joint_types = 1, name = "RSNAonly_wrists")
+model = RSNA_model.create_vgg_avg_rsna_model(configuration, "vvg_avg_wrists", no_joint_types = 1)
 
 model.summary()
 
-finetune_model(model, 'complex_model_RSNA_wrists_pretrain_without_NIH_pretrain_fixed_joints_moreaug', wrist_dataset, wrist_val_dataset, epochs_before = 0, epochs_after = 101, n_outputs = 1)
+finetune_model(model, 'vvg_avg_wrists', wrist_dataset, wrist_val_dataset, epochs_before = 0, epochs_after = 51, n_outputs = 1, is_wrists = True)
