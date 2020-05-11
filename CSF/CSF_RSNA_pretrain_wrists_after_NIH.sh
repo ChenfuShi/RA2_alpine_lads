84c0506c2b8ee3446ff51e7d0ba2402ff702f71d
Michael <michael.stadler@postgrad.manchester.ac.uk>
2020-05-11 07:54:51 +0100

    Added rsna wrist pretrain script

---
+++ CSF/CSF_RSNA_pretrain_wrists_after_NIH.sh
@@ -0,0 +1,24 @@
+#!/bin/bash --login
+#$ -cwd
+#$ -j y
+#$ -o logs
+#$ -l nvidia_v100
+#$ -pe smp.pe 8
+
+module load apps/anaconda3/5.2.0/bin
+
+export OMP_NUM_THREADS=$NSLOTS
+mkdir -p logs
+
+# check nvidia-smi
+nvidia-smi 
+
+## activate conda environment
+# this also includes cuda and cuda toolkits
+
+source activate /mnt/jw01-aruk-home01/projects/ra_challenge/tensorflow2.0_gpu
+
+rm ~/localscratch/* -r
+
+# run python training script
+python ../ra_joint_predictions/finetune_NIH_model_wrists_complex_on_RSNA.py
